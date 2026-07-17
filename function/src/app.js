0ded0c7796511084ffa3659abb2218ecea32154c
Yug1020 <yugandhardhore@gmail.com>
2026-07-17 09:29:15 +0530

    first commit

---
+++ function/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import http from "http";
+
+const app = express()
+
+app.use("/hello", (req, res) => {
+    res.send("hello welcome to nothing");
+});
+
+app.use("/", (req, res) => {
+    res.send("NamasteNode User")
+})
+
+app.listen(3000, () => {
+    console.log("Successfully started server")
+})
